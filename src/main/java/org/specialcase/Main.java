f02d23b091f024f368cb283ff8f92e39a61794c5
leesky <leehaneul0623@gmail.com>
2022-12-09 23:10:48 +0900

    chore : legacy 코드 작성

---
+++ src/main/java/org/specialcase/Main.java
@@ -0,0 +1,7 @@
+package org.specialcase;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/specialcase/legacy/LegacyStack.java
@@ -0,0 +1,47 @@
+package org.specialcase.legacy;
+
+public class LegacyStack {
+    private int size = 0;
+    private int capacity;
+    private int[] elements;
+
+    public LegacyStack(int capacity) {
+        this.capacity = capacity;
+        elements = new int[capacity];
+    }
+
+    public Boolean isEmpty() {
+        return size == 0;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void push(int element) {
+        if (size == capacity)
+            throw new Overflow();
+        this.elements[size++] = element;
+    }
+
+    public int pop() {
+        if (size == 0)
+            throw new Underflow();
+        return elements[--size];
+    }
+
+    public static LegacyStack make(int capacity) {
+        if (capacity < 0)
+            throw new IllegalCapacity();
+        return new LegacyStack(capacity);
+    }
+
+    public class Overflow extends RuntimeException {
+    }
+
+    public static class IllegalCapacity extends RuntimeException {
+    }
+
+    public class Underflow extends RuntimeException {
+    }
+}
